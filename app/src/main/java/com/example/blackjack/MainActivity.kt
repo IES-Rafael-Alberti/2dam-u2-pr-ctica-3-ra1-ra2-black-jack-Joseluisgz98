301f1041a2c1debb59a4d99e05ff751b2e86f674
joseluisgz98 <joseluis_cadiz98@hotmail.com>
2023-11-22 16:20:00 +0100

    parte1

--- app/src/main/java/com/example/blackjack/MainActivity.kt
+++ app/src/main/java/com/example/blackjack/MainActivity.kt
@@ -34,6 +34,7 @@ class MainActivity : ComponentActivity() {
                         composable(Routes.soloUnJugador.routes){SoloUnJugador(navController)}
                     }
 
+
                 }
             }
         }
